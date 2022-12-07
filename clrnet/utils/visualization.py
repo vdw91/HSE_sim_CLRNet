8cdb979e43ff5d3c223f9ccc9cbde3aef4533003
thptai <thptai.itc@gmail.com>
2022-12-07 11:59:23 +0900

    Update visualization.py
    Add imshow_lanes_cctv function

--- clrnet/utils/visualization.py
+++ clrnet/utils/visualization.py
@@ -62,3 +62,34 @@ def imshow_lanes(img, lanes, show=False, out_file=None, width=4):
         if not osp.exists(osp.dirname(out_file)):
             os.makedirs(osp.dirname(out_file))
         cv2.imwrite(out_file, img)
+        
+ def imshow_lanes_cctv(img, lanes, transformMat, show=False, out_file=None, width=4):
+    lanes_xys = []
+    for _, lane in enumerate(lanes):
+        xys = []
+        for x, y in lane:
+            if x <= 0 or y <= 0:
+                continue
+            x, y = int(x), int(y)
+            xys.append((x, y))
+        lanes_xys.append(xys)
+    lanes_xys.sort(key=lambda xys : xys[0][0])
+
+    for idx, xys in enumerate(lanes_xys):
+        for i in range(1, len(xys)):
+            cv2.line(img, xys[i - 1], xys[i], COLORS[idx], thickness=width)
+
+
+    if show:
+        cv2.imshow('view', img)
+        cv2.waitKey(0)
+    
+    transformMat_inv = np.linalg.inv(transformMat)
+
+    if out_file:
+        if not osp.exists(osp.dirname(out_file)):
+            os.makedirs(osp.dirname(out_file))
+        
+        resize_img = cv2.resize(img, (1640, 590))
+        warp_img = cv2.warpPerspective(resize_img, transformMat_inv, (1080, 1920))
+        cv2.imwrite(out_file, warp_img)
