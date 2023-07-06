4c332987c31c23bedeef988d55dc02bc43a99ecb
thptai <thptai.itc@gmail.com>
2023-07-06 16:27:40 +0900

    Add folder for Background Subtraction Module
    Provide a run file for the background subtraction module.
    Note: Need to implement functions inside.

---
+++ bg_subtraction_module/bg_subtraction_run.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+import argparse
+import numpy as np
+import random
+from tqdm import tqdm
+
+
+def frame_difference():
+    return
+
+def gaussian_mixtrue_model():
+    return
+
+def median_approximation():
+    return
+
+def single_gaussian():
+    return
+
+
+def main():
+    return 
+
+if __name__ == '__main__':
+    main()
