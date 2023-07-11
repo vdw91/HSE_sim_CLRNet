7c72769d919eb0a4156fc92fd3b3550b781eca0f
thptai <thptai.itc@gmail.com>
2023-07-11 22:01:15 +0900

    Update bg_subtraction_run.py
    - Refractor the background subtraction using median approximation.
    - Add YoloV8 for testing compatibility.

--- bg_subtraction_module/bg_subtraction_run.py
+++ bg_subtraction_module/bg_subtraction_run.py
@@ -5,7 +5,43 @@ import numpy as np
 import random
 # from tqdm import tqdm
 
+from ultralytics import YOLO
+
+
+# Video information
 video_path = 'downtown-rain-0003.mov'
+# video_path = 'highway-night-0005.mov'
+row, col, channel = 1280, 1920, 3
+frame_rate = 30
+
+
+# initialising background and foreground
+background = np.zeros([row,col],np.uint8)
+foreground = np.zeros([row,col],np.uint8)
+a = np.uint8([255])
+b = np.uint8([0])
+noise_remove_kernel = np.ones([3,3],np.uint8)
+
+
+def first_stage_bg_subtraction(frame):
+
+    # Convert frame to gray (1 channel)
+    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    # Apply algorithm of median approximation method to get estimated background
+    background = np.where(gray_frame>background,background+1,background-1)
+
+    # Use cv2.absdiff instead of background - frame, because 1 - 2 will give 255 which is not expected
+    foreground = cv2.absdiff(background,gray_frame)
+
+    # setting a threshold value for removing noise and getting foreground
+    foreground = np.where(foreground>40,a,b)
+            
+    # removing noise
+    foreground = cv2.erode(foreground,noise_remove_kernel)
+    foreground = cv2.dilate(foreground,noise_remove_kernel)
+    # using bitwise and to get colored foreground
+    foreground = cv2.bitwise_and(frame, frame, mask=foreground)
 
 
 
@@ -16,17 +52,6 @@ def main():
     row, col, channel = frame.shape
 
     print(row, col, channel)
-
-    # initialising background and foreground
-    background = np.zeros([row,col],np.uint8)
-    foreground = np.zeros([row,col],np.uint8)
-
-    # converting data type of intergers 0 and 255 to uint8 type
-    a = np.uint8([255])
-    b = np.uint8([0])
-
-    # creating kernel for removing noise
-    kernel = np.ones([3,3],np.uint8)
 
 
     # Check if camera opened successfully
@@ -43,22 +68,9 @@ def main():
             cv2.imshow('Frame',frame)
 
 
-            gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    
-            # applying algorithm of median approximation method to get estimated background
-            background = np.where(gray_frame>background,background+1,background-1)
-
-            # using cv2.absdiff instead of background - frame, because 1 - 2 will give 255 which is not expected
-            foreground = cv2.absdiff(background,gray_frame)
+            first_stage_bg_subtraction(frame)
             
-            # setting a threshold value for removing noise and getting foreground
-            foreground = np.where(foreground>40,a,b)
             
-            # removing noise
-            foreground = cv2.erode(foreground,kernel)
-            foreground = cv2.dilate(foreground,kernel)
-            # using bitwise and to get colored foreground
-            foreground = cv2.bitwise_and(frame, frame, mask=foreground)
             cv2.imshow('background',background)
             cv2.imshow('foreground',foreground)
         
